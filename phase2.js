a34d7749a0dff20ec8cca2a031637cf03993935c
曾俊菁 <1606681131@qq.com>
2024-06-03 16:11:26 +0800

    添加HTML标签，确保页面在移动端正确呈现，而不是以桌面宽度进行缩放。

---
+++ phase2.js
@@ -0,0 +1,10 @@
+var UI = {};
+    UI.appWidth = window.innerWidth > 800 ? 800 : window.innerWidth ;
+    UI.appHeight = window.innerHeight;
+ 	const LETTERS = 22 ;
+	const baseFont = UI.appWidth / LETTERS;
+ 
+    //通过把body对象的宽度和高度设置为设备/屏幕的宽度和高度，实现全屏。
+    //通过CSS对子对象百分比（纵向）的配合，从而实现响应式设计的目标。
+    document.body.style.width = UI.appWidth - 1*baseFont + "px" ;
+    document.body.style.height = UI.appHeight - 4*baseFont + "px";
