75c92f3ee318840efe70b57d42e6c8cc85534ba7
曾俊菁 <1606681131@qq.com>
2024-06-05 22:12:57 +0800

    项目第二版：窄屏终端的响应式设计

--- phase2.js
+++ phase2.js
@@ -3,7 +3,6 @@ var UI = {};
     UI.appHeight = window.innerHeight;
  	const LETTERS = 22 ;
 	const baseFont = UI.appWidth / LETTERS;
- 
     //通过把body对象的宽度和高度设置为设备/屏幕的宽度和高度，实现全屏。
     //通过CSS对子对象百分比（纵向）的配合，从而实现响应式设计的目标。
     document.body.style.width = UI.appWidth - 1*baseFont + "px" ;
